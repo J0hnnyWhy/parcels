ef991db960b38dc18685df1983ae0fecdf78482a
john young & steven francisco <student@epicodus.com>
2015-06-01 10:45:48 -0700

    it will add four arguments to parcel class

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,5 +1,25 @@
-class String
-  # define_method(:word_freq) do |string|
-  # counter["y"] +=1
-  # end
+class Parcel
+
+  define_method(:initialize) do |height, length, depth, weight|
+  @height = height
+  @length = length
+  @depth = depth
+  @weight = weight
+  end
+
+  define_method(:height) do
+    @height
+  end
+
+  define_method(:length) do
+    @length
+  end
+
+  define_method(:depth) do
+    @depth
+  end
+
+  define_method(:weight) do
+    @weight
+  end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,12 +1,36 @@
 require('rspec')
 require "pry"
-require('rps')
+require('parcels')
 require "capybara/rspec"
 require "./app"
 Capybara.app = Sinatra::Application
 
-describe('String#word_freq') do
-  it("takes the users string and splits it into individual words that are pushed into a hash") do
-    expect(("the the").word_freq()).to(eq("the" "the"))
+describe(Parcel) do
+  describe('#height') do
+  it("should be able to create a new parcel and specify the height in the arguments.") do
+    new_parcel = Parcel.new("3", "4", "5", "10")
+    expect(new_parcel.height()).to(eq("3"))
   end
+end
+
+describe('#length') do
+  it("should be able to create a new parcel and specify the length in the arguments.") do
+    new_parcel = Parcel.new("3", "4", "5", "10")
+    expect(new_parcel.length()).to(eq("4"))
+  end
+end
+
+describe('#depth') do
+  it("should be able to create a new parcel and specify the depth in the arguments.") do
+    new_parcel = Parcel.new("3", "4", "5", "10")
+    expect(new_parcel.depth()).to(eq("5"))
+  end
+end
+
+describe('#weight') do
+  it("should be able to create a new parcel and specify the weight in the arguments.") do
+    new_parcel = Parcel.new("3", "4", "5", "10")
+    expect(new_parcel.weight()).to(eq("10"))
+  end
+end
 end
